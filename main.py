f806169231fa002f928e291269edce525dfeda33
PC <nebgiorgi12@gmail.com>
2023-06-30 22:15:34 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def Dictionary():
+
+    Dict = {
+
+        '1':2012,
+        '2':1999,
+        '3':3000,
+        '4':5125,
+        '5':920
+
+        }
+
+
+    return min(Dict.values())
+
+print(Dictionary())
+
+
